510d286e6c0f3ab908c9374ff7c6215c782fdf0c
MatrXIdzi <wiczi03@interia.pl>
2024-03-20 22:46:43 +0100

    AES (subBytes, shiftRows), FileReader

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,40 @@
 package org.example;
 
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws Exception {
+
+        int[][] box = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+
+        printBox(box);
+
+        int[][] chBox = AES.subBytes(box);
+
+        printBox(chBox);
+
+//        FileReader fr = new FileReader("sample.pdf");
+//
+//        FileOutputStream fos = new FileOutputStream("coded.pdf");
+//        fr.read();
+//        byte[] b = fr.getBytes();
+//
+//        System.out.println(b.length);
+//        System.out.println();
+//        for(int i = 0; i < b.length; i++) {
+//            System.out.println(Integer.toBinaryString(b[i] & 0xFFFF));
+//        }
     }
+
+    private static void printBox(int[][] box) {
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+                System.out.print(box[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
 }
